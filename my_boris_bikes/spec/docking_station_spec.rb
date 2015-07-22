f1d3789ac3d79eda2ce314b9561e30dc54c7a200
Josh <Tattsy@hotmail.co.uk>
2015-07-22 10:46:20 +0100

    created test for working?

--- my_boris_bikes/spec/docking_station_spec.rb
+++ my_boris_bikes/spec/docking_station_spec.rb
@@ -2,4 +2,9 @@ require "docking_station"
 
 describe DockingStation do 
 	it { is_expected.to respond_to :release_bike }
+
+	it "Released bike expected to be working" do
+		bike = subject.release_bike
+		expect(bike).to respond_to :working?
+	end
 end
