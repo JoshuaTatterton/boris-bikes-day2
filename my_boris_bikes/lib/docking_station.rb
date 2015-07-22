32f2614540531731ec501130b6b114b5e6fed129
Josh <Tattsy@hotmail.co.uk>
2015-07-22 12:51:17 +0100

    modified station to hold 20 bikes

--- my_boris_bikes/lib/docking_station.rb
+++ my_boris_bikes/lib/docking_station.rb
@@ -1,13 +1,17 @@
 require_relative 'bike'
 
 class DockingStation
+  def initialize
+  	@bike = []
+  end
+
   def release_bike
-  	fail "No bikes available" unless @bike
-  	 @bike
+  	fail "No bikes available" if @bike.length <= 0
+  	 @bike.pop
   end
 
   def dock(i)
-  	fail "Station is full" if @bike
-  	@bike = i
+  	fail "Station is full" if @bike.length >= 20
+  	@bike << i
   end
 end
