3e67407983f56ee1fb440a1d399f2f32304090a8
Edwardyao123 <zhigang_edward@hotmail.com>
2015-07-22 13:13:27 +0100

    refactor count

--- my_boris_bikes/lib/docking_station.rb
+++ my_boris_bikes/lib/docking_station.rb
@@ -6,12 +6,23 @@ class DockingStation
   end
 
   def release_bike
-  	fail "No bikes available" if @bike.length <= 0
+  	fail "No bikes available" if empty?
   	 @bike.pop
   end
 
   def dock(i)
-  	fail "Station is full" if @bike.length >= 20
+  	fail "Station is full" if full?
   	@bike << i
   end
+
+  private 
+
+  def empty?
+  	@bike.empty?
+  end
+
+  def full?
+  	@bike.length >= 20
+  end
+
 end
