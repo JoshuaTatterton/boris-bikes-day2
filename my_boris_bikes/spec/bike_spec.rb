d53b0b50d46f5df8b95dc5f52ec7f5e8a7f28ee1
Edwardyao123 <zhigang_edward@hotmail.com>
2015-07-22 15:28:51 +0100

    add report broken test

--- my_boris_bikes/spec/bike_spec.rb
+++ my_boris_bikes/spec/bike_spec.rb
@@ -2,4 +2,9 @@ require 'bike'
 describe Bike do
   it { is_expected.to respond_to :working? }
   it { is_expected.to respond_to :report_broken}
+  
+  it 'report broken, bikes should not be working' do
+  	subject.report_broken
+  	expect(subject.working?).to eq false
+  end
 end
